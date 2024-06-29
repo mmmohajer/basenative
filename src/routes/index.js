078b2a6334762345290f7573c175555f4407526b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-06-29 12:49:12 -0400

    Screen page is working fine

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,6 @@ import Icon from 'BaseComponents/ReusableComps/Icon';
 
 import Home from 'Screens/Home';
 import Products from 'Screens/Products';
-import ScreenContainer from 'Components/ScreenWrappers/ScreenContainer';
-import DevDesign from 'Components/DevDesign';
 
 import {ProductNav} from './stackRoutes';
 import {LoginRegisterNav} from './stackRoutes';
@@ -20,11 +18,81 @@ const Routes = () => {
 
   return (
     <>
-      <NavigationContainer>
-        <ScreenContainer>
-          <DevDesign />
-        </ScreenContainer>
-      </NavigationContainer>
+      {isAuthenticated?.authenticated ? (
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+              tabBarActiveTintColor: 'dodgerblue',
+              tabBarItemStyle: {
+                padding: 4,
+              },
+            }}>
+            <Tab.Screen
+              name="Home"
+              component={Home}
+              options={{
+                title: 'Home',
+                tabBarIcon: ({size, color}) => (
+                  <Icon name="email" size={size} color={color} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="AddProduct"
+              component={Products}
+              options={{
+                title: 'Add Products',
+                tabBarLabelStyle: {display: 'none'},
+                tabBarItemStyle: {
+                  backgroundColor: 'transparent',
+                },
+                tabBarIconStyle: {
+                  width: 50,
+                  backgroundColor: 'tomato',
+                  height: 100,
+                  borderRadius: 50,
+                  top: -10,
+                },
+                tabBarIcon: ({size, color}) => (
+                  <Icon name="email" size={40} color={'white'} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="ProductStack"
+              component={ProductNav}
+              options={{
+                title: 'Products',
+                tabBarIcon: ({size, color}) => (
+                  <Icon name="email" size={size} color={color} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      ) : (
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarItemStyle: {
+                display: 'none',
+              },
+              tabBarStyle: {
+                display: 'none',
+              },
+              tabBarLabelStyle: {
+                display: 'none',
+              },
+              tabBarIconStyle: {
+                display: 'none',
+              },
+              headerShown: false,
+            }}>
+            <Tab.Screen name="LoginStack" component={LoginRegisterNav} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      )}
     </>
   );
 };
